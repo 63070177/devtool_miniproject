939648af988a1fd9a41158882579d13e52c693a9
63070177 <63070177@kmitl.ac.th>
2023-04-16 21:57:50 +0700

    brown

--- hello-app/src/App.js
+++ hello-app/src/App.js
@@ -1,15 +1,20 @@
-import axios from "axios";
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+import StyleSheet from "react-native";
 
 function App() {
   const hostname = `${window.location.hostname}`;
   console.log(hostname);
   return (
     <div className="container">
-      <ul style={text}>1</ul>
+      <ul style={styles.text}>1</ul>
     </div>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'brown'
+  }
+})
 
 export default App;
