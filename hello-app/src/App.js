8e55caf9aa10b3a854d0f4537f652bb0badc3518
63070177 <63070177@kmitl.ac.th>
2023-04-17 13:08:08 +0700

    test9

--- hello-app/src/App.js
+++ hello-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import logo from './logo.png';
 
 function App() {
   const hostname = `${window.location.hostname}`;
@@ -7,7 +8,7 @@ function App() {
   return (
     <div>
       <div className="logo">
-          <img src="logo.png"></img>
+          <img src={logo}></img>
         </div>
       <div>
         <h1 className="AppName">LaSistant</h1>
