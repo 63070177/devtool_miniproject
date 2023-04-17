c92b5cbf47d4e0d35a212d9cfedb932592bb1afe
63070177 <63070177@kmitl.ac.th>
2023-04-17 14:11:07 +0700

    test32

--- hello-app/src/App.js
+++ hello-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import logo from './logo.png';
+import logo from "./logo.png";
 
 function App() {
   const hostname = `${window.location.hostname}`;
@@ -8,16 +8,24 @@ function App() {
   return (
     <div>
       <div className="tab">
-          <img src={logo} className="logo"></img>
-          <div className="mainmenu">
-            <button><b>Home</b></button>
-            <button><b>Log In</b></button>
-            <button><b>Register</b></button>
-          </div>
+        <img src={logo} className="logo"></img>
+        <div className="mainmenu">
+          <button>
+            <b>Home</b>
+          </button>
+          <button>
+            <b>Log In</b>
+          </button>
+          <button>
+            <b>Register</b>
+          </button>
         </div>
+      </div>
       <div className="container">
         <h1 className="AppName">LaSistant</h1>
         <p className="text">Let's begin</p>
+      </div>
+      <div className="bottomright">
         <div className="rect1"></div>
         <div className="rect2"></div>
         <div className="rect3"></div>
