b5977bec87baddc441ddaf185027ee76ec4acba1
63070177 <63070177@kmitl.ac.th>
2023-04-17 12:45:55 +0700

    test5

--- hello-app/src/App.js
+++ hello-app/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import styles from './App.css';
+import './App.css';
 
 function App() {
   const hostname = `${window.location.hostname}`;
   console.log(hostname);
   return (
     <div className="container">
-      <h1 className="text">LaSistant</h1>
+      <div className="logo">
+        <img src=""></img>
+      </div>
+      <h1 className="AppName">LaSistant</h1>
       <p className="text">Let's begin</p>
     </div>
   );
